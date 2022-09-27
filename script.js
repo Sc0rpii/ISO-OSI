1e174990695e9ee78e17f3250178672ae21f5832
Sc0rpii <mirkofreschi66@gmail.com>
2022-09-27 09:55:39 +0200

    Layer upgrade

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var layer = document.querySelectorAll(".layer");
-
+function switchPage(index){
+    switch (index){
+        case 0: window.location = "./layer/applicationd.html";
+        break;
+        case 1: window.location = "./layer/presentation.html";
+        break;
+        case 2: window.location = "./layer/session.html";
+        break;
+        case 3: window.location = "./layer/transport.html";
+        break;
+        case 4: window.location = "./layer/network.html";
+        break;
+        case 5: window.location = "./layer/datalink.html";
+        break;
+        case 6: window.location = "./layer/physical.html";
+        break;
+    }
+}
